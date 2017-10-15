78bca355ac3a6ebaee975fcc325b17cb3290c037
Wojtek <wojciech.rozycki@cybercom.com>
2017-10-15 21:39:14 +0200

    Kamera plus postac

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+        transform.rotation = Quaternion.Euler(0, 0, player.transform.rotation.z);
+    }
+}
