f975d34c35eff9d720e70708266281762f5da414
lipowski195646 <lipa422@gmail.com>
2017-10-16 21:08:17 +0200

    Dodanie podstawowego sterowania za pomoca przyciskow A i D oraz skakania za pomoca spacji; dodanie zabezpieczenia, ktore zabrania skakania w locie.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float moveSpeed;
+	public float rotateSpeed;
+	public float jumpForce;
+
+	private float distToGround;
+
+	private SphereCollider myCollider;
+	private Rigidbody myRigidbody;
+
+	bool IsGrounded(){
+		return Physics.Raycast(transform.position, -Vector3.up, distToGround);
+	}
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody>();
+		myCollider = GetComponent<SphereCollider>();
+		distToGround = myCollider.bounds.extents.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myRigidbody.velocity = new Vector3 (myRigidbody.velocity.x, myRigidbody.velocity.y, moveSpeed);
+
+		if(Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+		{
+			myRigidbody.velocity = new Vector3 (myRigidbody.velocity.x, jumpForce, myRigidbody.velocity.z);
+		}
+
+		if(Input.GetKey(KeyCode.A))
+		{
+			myRigidbody.velocity = new Vector3 (-rotateSpeed, myRigidbody.velocity.y, myRigidbody.velocity.z);
+		}
+
+		if(Input.GetKey(KeyCode.D))
+		{
+			myRigidbody.velocity = new Vector3 (rotateSpeed, myRigidbody.velocity.y, myRigidbody.velocity.z);
+		}
+			
+}
+}
